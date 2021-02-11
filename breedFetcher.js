ba46ee393edb5993ad8406bd8b8e839bceccd8d1
Ghulam Murtaza <ghulam.murtaza@mail.mcgill.ca>
2021-02-11 10:29:08 +0000

    file created

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,25 @@
 const request = require('request');
+
+const args = process.argv.slice(2);
+
+const breedFetcher = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      return callback("Breed not found");
+    }
+    return callback(null, data[0].description);
+  });
+};
+breedFetcher(args[0], (error, desc) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(desc);
+  }
+});
+
+
